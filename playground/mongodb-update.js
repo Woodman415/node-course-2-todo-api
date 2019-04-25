9987bb6e2b5fc6fb403a1611d9da83cde5a67365
Duane Wood <duane.wood.yyc@gmail.com>
2019-04-25 12:15:59 +0300

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,26 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    console.log('Unable to connect to MongoDB server')
+  }
+  console.log('Connected to MongoDB server');
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5cc16318fdd1212d98ad9b17')
+  }, {
+    $set: {
+    Name: 'Duane'
+  },
+    $inc:{
+      age: 1
+    }
+  }, {
+      returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // db.close();
+});
